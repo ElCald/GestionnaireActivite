850481ca31d9cd446b6efcb27520bbc6845ea4c2
ElCald <eliot.cal@gmail.com>
2022-11-30 16:00:37 +0100

    fix all

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'admin',
     ];
 
     /**
@@ -58,4 +59,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function enfants() {
+        return $this->hasMany(Enfant::class);
+    }
+
 }
